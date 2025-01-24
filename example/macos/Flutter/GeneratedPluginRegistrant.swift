083c3c14c36902ffa2ab6fd330cea54f942f6931
NoealRajeev <noealrajeev987@gmail.com>
2025-01-24 21:41:57 +0530

    Example sun configuration

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_ar_vr
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterArVrPlugin.register(with: registry.registrar(forPlugin: "FlutterArVrPlugin"))
+}
---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_ar_vr/flutter_ar_vr_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterArVrPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterArVrPluginCApi"));
+}
